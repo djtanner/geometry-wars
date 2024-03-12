8c18642a85bda39cc1bd2565a126a9013d73cd1f
Zhila Shariat <zshariat08@gmail.com>
2024-03-11 23:33:22 -0700

    set up components header file

---
+++ Components.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Vec2.h"
+#include <SFML/Graphics.hpp>
+
+class CTransform
+{
+public:
+    Vec2 pos = {0.0, 0.0};
+    Vec2 velocity = {0.0, 0.0};
+    float angle = 0;
+
+    CTransform(const Vec2 &p, const Vec2 &v, float a) : pos(p), velocity(v), angle(a) {}
+};
+
+class CShape
+{
+public:
+    sf::CircleShape circle;
+
+    CShape(float radius, int points, const sf::Color &fill, const sf::Color &outline, float thickness)
+        : circle(radius, points)
+    {
+
+        circle.setFillColor(fill);
+        circle.setOutlineColor(outline);
+        circle.setOutlineThickness(thickness);
+        circle.setOrigin(radius, radius);
+    }
+};
