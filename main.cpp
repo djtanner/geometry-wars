aac3316acda7af7964dcf1d52a8bd4290a967bc6
Zhila Shariat <zshariat08@gmail.com>
2024-03-11 23:27:07 -0700

    entity header file setup

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <SFML/Graphics.hpp>
-
 #include "Game.h"
+#include <iostream>
 
 int main()
 {
+
+    // testing vec2
+    Vec2 v1(100, 100);
+    Vec2 v2(200, 200);
+    Vec2 v3 = v1 + v2;
+
+    std::cout << v3.x << " " << v3.y << std::endl;
+
+    // main game loop
     Game g("config.txt");
 
     g.run();
